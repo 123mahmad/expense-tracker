2259f5fa63ece009baf10bde32d5efb7bdc3a1ed
mahmad <ahmadqwerty123@gmail.com>
2022-08-14 02:31:56 +0500

    Add CustomGridToolbar, styling, width to DataTable

--- src/Components/BookHistory.js
+++ src/Components/BookHistory.js
@@ -1,11 +1,11 @@
 import { Delete, Edit } from '@mui/icons-material';
-import { Container } from '@mui/material';
-import { DataGrid, GridActionsCellItem, GridToolbar } from '@mui/x-data-grid';
+import { Container, Typography } from '@mui/material';
+import { DataGrid, GridActionsCellItem, GridToolbarColumnsButton, GridToolbarContainer, GridToolbarDensitySelector, GridToolbarExport, GridToolbarFilterButton, GridToolbarQuickFilter } from '@mui/x-data-grid';
 import React, { useContext } from 'react'
 import { Context } from '../App'
 import { deleteTransaction } from '../firebase';
 
-export const BookHistory = () => {
+export const BookHistory = ({user}) => {
 
   let {transactions, currentBook, setTransaction, editTransMode, setEditTransMode} = useContext(Context);
   
@@ -18,11 +18,11 @@ export const BookHistory = () => {
   };
 
   let columns = [
-    { field: 'time', headerName: 'Time', type:'dateTime', flex: 1, maxWidth: 180},
-    { field: 'moneyFlow', headerName: 'Flow', type:'singleSelect', flex: 1, maxWidth: 60, valueOptions: ['in','out']},
-    { field: 'amount', headerName: 'Amount', type: 'number', flex: 1, maxWidth: 75},
-    { field: 'name', headerName: 'Name', flex: 1},
-    { field: 'details', headerName: 'Details', flex: 2.5},
+    { field: 'time', headerName: 'Time', type:'dateTime', width: 180},
+    { field: 'moneyFlow', headerName: 'Flow', type:'singleSelect', width: 65, valueOptions: ['in','out']},
+    { field: 'amount', headerName: 'Amount', type: 'number', width: 75},
+    { field: 'name', headerName: 'Name', minWidth:150, flex:1},
+    { field: 'details', headerName: 'Details', minWidth:200, flex:3},
     { field: 'actions', headerName:'Actions', type:'actions', width:70,
       getActions: (params) => [
         <GridActionsCellItem
@@ -45,29 +45,51 @@ export const BookHistory = () => {
     return transaction.bookId === currentBook.id;
   });
 
+  function CustomToolbar() {
+    return (
+      <div>
+        <GridToolbarContainer>
+          <GridToolbarColumnsButton/>
+          <GridToolbarDensitySelector/>
+          <GridToolbarFilterButton/>
+          <GridToolbarExport
+            printOptions={{
+              hideFooter: true,
+              hideToolbar: true,
+              fields: ['time', 'moneyFlow', 'amount', 'name', 'details']
+            }}
+          />
+          <GridToolbarQuickFilter
+            sx={{marginLeft: 'auto'}}
+          />
+        </GridToolbarContainer>
+        <Typography>Logged User: {user.displayName} ({user.email})</Typography>
+        <Typography>{}</Typography>
+      </div>
+    );
+  };
+
   return (
     <Container>
-      <h5>Book History</h5>
-      <div style={{minHeight: 400, width: '100%'}}>
+      <h5 style={{marginBottom:10}}>Book History</h5>
+      <div style={{minHeight: 100, borderRadius:'7px', border:'1px solid rgba(0,0,0,0.3)'}}>
         <DataGrid
+          sx={{
+            border: 0,
+            borderColor: 'primary.light',
+            '& .MuiDataGrid-cell:hover': {
+              color: 'primary.main',
+            },
+          }}
           rows={rows}
           columns={columns}
           rowsPerPageOptions={[5, 50, 100, 500, 1000]}
           autoHeight
           components = {{
-            Toolbar: GridToolbar,
-          }}
-          componentsProps = {{
-            toolbar: {
-              printOptions:
-              {
-                hideFooter: true,
-                hideToolbar: true
-              }
-            }
+            Toolbar: CustomToolbar,
           }}
         />
       </div>
     </Container>
   )
-}
+};
